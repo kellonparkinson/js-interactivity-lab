a3b791b5dc5d1cbe1be0e6f29c6daa94612484e9
Kellon Parkinson <kellon.digital@gmail.com>
2022-10-12 17:12:15 -0600

    completed full lab

--- index.js
+++ index.js
@@ -28,16 +28,24 @@ document.querySelector('form').addEventListener('submit', addMovie)
 const deleteMovie = event => {
     event.target.parentNode.remove()
 
-    message.textContent = 'Movie deleted!'
+    message.textContent = `${event.target.parentNode.firstChild.textContent} deleted!`
+
+    revealMessage()
 }
 
 const crossOffMovie = event => {
-    event.target.classList.toggle('.checked')
+    event.target.classList.toggle('checked')
 
-    if (event.target.classList.contains('.checked')) {
-        message.textContent = 'Movie Watched!'
+    if (event.target.classList.contains('checked')) {
+        message.textContent = `${event.target.textContent} watched!`
     } else {
-        message.textContent = 'Movie added back!'
+        message.textContent = `${event.target.textContent} added back!`
     }
+    revealMessage()
 }
 
+const revealMessage = () => {
+    message.classList.remove('hide')
+
+    setTimeout((() => message.classList.add('hide')), 1000)
+}
